80043d908de9d35ad4612a2cbeb18a5c44141b10
Chris N <hi.chrisn@gmail.com>
2022-02-02 14:29:07 -0300

    express routers

---
+++ routes/friends.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const friendsController = require('../controllers/friends.controller');
+
+const friendsRouter = express.Router();
+
+// app.get('/', (req, res) => {
+//   res.json(friends);
+// });
+
+friendsRouter.use((req, res, next) => {
+  console.log('ip address:', req.ip);
+  next();
+});
+
+friendsRouter.get('/', friendsController.getFriends);
+friendsRouter.get('/:friendId', friendsController.getFriend);
+friendsRouter.post('/', friendsController.postFriend);
+
+module.exports = friendsRouter;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const friendsController = require('./controllers/friends.controller');
-const messagesController = require('./controllers/messages.controller');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
@@ -12,21 +12,13 @@ app.use((req, res, next) => {
   next();
   // actions go here...
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.json(friends);
-// });
-
-app.get('/friends', friendsController.getFriends);
-app.get('/friends/:friendId', friendsController.getFriend);
-app.post('/friends', friendsController.postFriend);
-
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
