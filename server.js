28c34d9f39ec01b05e5f1161b02e52dee7d038f7
Chris N <hi.chrisn@gmail.com>
2022-02-02 11:42:14 -0300

    post requests in express

--- server.js
+++ server.js
@@ -19,6 +19,32 @@ const friends = [
 //   res.json(friends);
 // });
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  // actions go here...
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing friend name'
+    });
+  }
+
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length
+  };
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
+
 app.get('/friends', (req, res) => {
   res.json(friends);
 });
@@ -30,7 +56,7 @@ app.get('/friends/:friendId', (req, res) => {
     res.status(200).json(friend);
   } else {
     res.status(404).json({
-      error: "Friend does not exist"
+      error: 'Friend does not exist'
     });
   }
 });
